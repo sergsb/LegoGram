5b51ed2d3a833ffbff93acaf4fc133cea38424cf
Sergey Sosnin <serg.sosnin@gmail.com>
2020-08-03 16:43:32 +0300

    Update rnn_sampler.py

--- legogram/apps/rnn_sampler.py
+++ legogram/apps/rnn_sampler.py
@@ -1,22 +1,28 @@
 from legogram.base import *
+import numpy as np 
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0) # only difference
 
 class LegoGramRNNSampler():
-    def __init__ (self, lg):
+    def __init__ (self, lg,optimize=True):
         self.lg = lg
+        self.optimize = optimize
         self.special_tokens = ['<pad>', '<bos>', '<eos>']
         self.nspecs = len(self.special_tokens)
         self.vocsize = self.lg.vocsize+self.nspecs
-    
+
     def encode (self, smi):
-        return [c+self.nspecs for c in self.lg.encode(smi)]
+        return [c+self.nspecs for c in self.lg.encode(smi,optimize=self.optimize)]
     
     def decode (self, code):
         return self.lg.decode([c - self.nspecs for c in code])
-        #return self.lg.decode([c-self.nspecs for c in code])
     
     def get_compat_rules (self, graph): # mask only
         rule_mask = self.lg.get_compat_rules(graph, as_mask=True)
-        return rule_mask#np.hstack((np.ones((self.nspecs), np.float32), rule_mask))
+        return rule_mask
     
     def init_state (self, batch_size):
         graphs = [None] * batch_size
@@ -24,6 +30,7 @@ class LegoGramRNNSampler():
         return (graphs, finished)
     
     def sample(self, state, logits, placeholder_idx):
+        placeholder_idx=[-3,-2,-1]
         graphs, finished = state
         batch_len = logits.shape[0]
         logits = logits[:,self.nspecs:]
@@ -34,24 +41,22 @@ class LegoGramRNNSampler():
                 mask = self.get_compat_rules(graph)
                 if mask.sum() == 0.:
                     finished[i] = True
-                    current[i] = placeholder_idx
+                    current[i] = placeholder_idx[2]
                     continue
-                
                 logits_of_a_rule = logits[i] * mask
                 bool_mask = mask.astype(np.bool)
                 prob = softmax(logits_of_a_rule[bool_mask])
                 current[i] = np.random.choice(np.array(range(logits.shape[-1]))[bool_mask], 1, p=prob)[0]
-                if not check_compat(graphs[i], self.lg.rules[current[i]]):
+                if not check_compat(graphs[i].graph, list(self.lg.rules.keys())[list(self.lg.rules.values()).index(current[i])].graph):
                     raise Exception(
-                        "Failed to combine rules! {} and {}".format(graphs[i], self.lg.rules[current[i]]))
-                graphs[i] = combine_rules(graphs[i], self.lg.rules[current[i]])
+                        "Failed to combine rules! {} and {}".format(graphs[i], list(self.lg.rules.keys())[list(self.lg.rules.values()).index(current[i])]))
+                graphs[i] = Rule(combine_rules(graphs[i].graph, list(self.lg.rules.keys())[list(self.lg.rules.values()).index(current[i])].graph))
             
             elif (not graph):
                 prob = softmax(logits[i])
                 current[i] = np.random.choice(range(logits.shape[-1]), 1, p=prob)[0]
-                graphs[i] = self.lg.rules[current[i]]
+                graphs[i] = list(self.lg.rules.keys())[list(self.lg.rules.values()).index(current[i])]
             else:
-                current[i] = placeholder_idx
+                current[i] = placeholder_idx[0]
         state = (graphs, finished)
-        return state, [c + self.nspecs for c in current]
-
+        return state, np.array([c + self.nspecs for c in current])
